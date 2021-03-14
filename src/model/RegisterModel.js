6191e2334d3b8ab45d85caa99fee4be02ec7b961
henriquetty <gamercaipira@gmail.com>
2021-03-13 22:47:13 -0300

    fixing connection with database crashing application

--- src/model/RegisterModel.js
+++ src/model/RegisterModel.js
@@ -1,7 +1,26 @@
-const connection = require("../database/dbConnection");
+const mysql = require("mysql");
+const connectionConfig = require("../database/dbConnection");
 
 class RegisterModel {
-  register(data, callback) {
+  static verifyEmailExists(email, callback) {
+    let connection = mysql.createConnection(connectionConfig);
+
+    let sql = `SELECT email FROM user_account WHERE email = '${email}';`;
+
+    connection.query(sql, (error, results, fields) => {
+      if (error) {
+        callback(error, null, null);
+      } else {
+        callback(null, results, fields);
+      }
+    });
+
+    connection.end();
+  }
+
+  static register(data, callback) {
+    let connection = mysql.createConnection(connectionConfig);
+
     const {
       fname,
       lname,
@@ -12,18 +31,19 @@ class RegisterModel {
       gender,
     } = data;
 
-    //bcrypt needed
     let sql = `INSERT INTO user_account (first_name, last_name, email, password, age, gender)
     VALUES ('${fname}', '${lname}', '${email}', '${password}', '${birthdate}', '${gender}')`;
 
     connection.query(sql, (error, results, fields) => {
       if (error) {
-        callback(error, null);
+        return callback(error, null, null);
       }
 
-      callback(null, results);
+      callback(null, results, fields);
     });
+
+    connection.end();
   }
 }
 
-module.exports = new RegisterModel();
+module.exports = RegisterModel;
