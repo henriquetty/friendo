c86a35dae2e6a04350a9054b8b95ac395688b9d0
henriquetty <gamercaipira@gmail.com>
2021-03-29 22:37:04 -0300

    fixed async problem with database

--- src/controller/registerController.js
+++ src/controller/registerController.js
@@ -1,31 +1,45 @@
+const { body, validationResult } = require("express-validator");
 const RegisterModel = require("../model/RegisterModel");
 
 module.exports = {
-  render: async function (req, res) {
+  render: function (req, res) {
     res.render("register");
   },
+  validation: [body("email").isEmail().normalizeEmail(), body("password").isLength({ min: 8 })],
   register: async function (req, res) {
-    let email, isEmailRegistered;
+    const validationErrors = validationResult(req);
 
-    email = req.body.email;
+    if (!validationErrors.isEmpty()) {
+      req.flash(
+        "validationErrors",
+        "You must use a valid e-mail and your password has to be 8 chars or more."
+      );
+      return res.redirect("/register?success=false&code=invalidFields");
+    }
 
-    //to fix
-    const emailsRegistered = (callback) => {
-      RegisterModel.verifyEmailAvailable(email, (results) => {
-        return callback(results.length);
-      });
-    };
+    try {
+      let isEmailRegistered = await RegisterModel.verifyEmailAvailable(req.body.email);
 
-    testUserEmail((i) => {
-      console.log(i);
-    });
+      if (isEmailRegistered) {
+        req.flash("emailTaken", "This email is not available.");
+        return res.redirect("/register?success=false&code=emailtaken");
+      }
 
-    // RegisterModel.register(req.body, (error, results, fields) => {
-    //   console.log(results.affectedRows);
-    // });
-
-    res.json({
-      ok: "ok",
-    });
+      //in case isEmailRegistered is false
+      RegisterModel.register(req.body)
+        .then((data) => {
+          req.flash("regSuccess", "You may sign in now :)");
+          res.redirect("/login");
+        })
+        .catch((err) => {
+          console.log(err);
+          req.flash("genericError", "Administrators were warned of this error.");
+          res.redirect("/register?success=false");
+        });
+    } catch (err) {
+      console.log(err);
+      req.flash("genericError", "Administrators were warned of this error.");
+      res.redirect("/register?success=false");
+    }
   },
 };
